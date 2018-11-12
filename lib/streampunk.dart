44978dafadffd26eaab44480b4288e653cbba9cc
akersof <sofiane@kiwitch.com>
2018-11-12 18:17:05 +0100

    add ioController.dart export

--- lib/streampunk.dart
+++ lib/streampunk.dart
@@ -4,5 +4,7 @@
 library streampunk;
 
 export 'src/streampunk_base.dart';
+export 'src/bloc.dart';
+export 'src/ioController.dart';
 
 // TODO: Export any libraries intended for clients of this package.
