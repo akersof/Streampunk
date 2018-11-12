f240099e5b254e95b750e657682ac79da1c7719a
akersof <sofiane@kiwitch.com>
2018-11-12 18:18:58 +0100

    a little flawless working example. nothing special actually

--- example/streampunk_example.dart
+++ example/streampunk_example.dart
@@ -1,6 +1,18 @@
 import 'package:streampunk/streampunk.dart';
 
+class Counter {
+  int _counter;
+  Counter([this._counter = 0]);
+  int get count => _counter;
+  void increment([int inc = 1]) => ++_counter;
+  void decrement([int dec = -1]) => increment(dec);
+}
+
+
 main() {
-  var awesome = new Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  final bloc = Bloc(state: Counter());
+  bloc.addInput("increment", InputController<int>());
+  bloc.addOutput("count", OutputController<int>());
+  bloc.getInput("increment").add(10);
+
 }
